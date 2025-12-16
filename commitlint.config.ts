3ff3b5a65d944042fb2890e13b8ca53b54abcb04
Sudheer Kumar <support@sudheer.ai>
2025-12-16 16:36:20 +0530

    style: format commitlint configuration for improved readability

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,23 +1,23 @@
 export default {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "type-enum": [
-      2,
-      "always",
-      [
-        "feat",
-        "fix",
-        "docs",
-        "style",
-        "refactor",
-        "perf",
-        "test",
-        "build",
-        "ci",
-        "chore",
-        "revert",
-      ],
-    ],
-    "subject-case": [0], // Allow any case for subject
-  },
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "type-enum": [
+            2,
+            "always",
+            [
+                "feat",
+                "fix",
+                "docs",
+                "style",
+                "refactor",
+                "perf",
+                "test",
+                "build",
+                "ci",
+                "chore",
+                "revert",
+            ],
+        ],
+        "subject-case": [0], // Allow any case for subject
+    },
 };
